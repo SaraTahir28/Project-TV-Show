77d5b9b9b75ab2ea0b7418ce25e4ffd31551de88
Sara Tahir <saratahir34@gmail.com>
2025-07-25 15:53:24 +0100

    Amending function to add Episode Code

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
+//Amending function to add Episode Code- (Episode Season and Episode name.)
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   episodeList.forEach((episode) => {
     const episodeDiv= document.createElement("div");
     rootElem.appendChild(episodeDiv);
-    episodeDiv.textContent= episode.name;
-  });
+    const season = String(episode.season).padStart(2, "0");
+    const number = String(episode.number).padStart(2, "0");
+    const episodeCode =`S${season}E${number}`;
+    episodeDiv.textContent = `${episodeCode} - ${episode.name}`;
+        
 }
+)}
 
 
 window.onload = setup;
